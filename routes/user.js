392925fb54d4d9fa8460a7a1c11a8348b2b6fe00
Caio <caiomora9@gmail.com>
2020-07-28 23:43:12 -0300

    (fix) removendo linhas em branco

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,6 @@ module.exports = app => {
     //retornar todos os usuários
     app.route('/api/v1/user').get(userController.getUser);
     
-
     //criar um novo usuário
     app.route('/api/v1/user').post(userController.createUser);
 }
